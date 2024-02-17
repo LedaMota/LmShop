ec83d2c30e444b65ad1d9d44ea8a7111bcbd5ecc
leda201232 <leidymotastz@outlook.com>
2024-02-17 19:05:22 -0300

    ajuste no responsivo do website

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-let search = document.querySelector('.search-box')
+// let search = document.querySelector('.search-box')
 
-document.querySelector('#search-icon') .onclik = () =>{
-    search.classList.toggle('active');
+// document.querySelector('#search-icon') .onclik = () =>{
+//     search.classList.toggle('active');
+//     // navbar.classList.remove('active');
+// }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');   
+    // search.classList.remove('active');
+   
+   
 }
+
+window.oncroll = () => {
+    navbar.classList.remove('active'); 
+}
+
+
+//Header Animation
+let header = document.querySelector('header');
+
+windows.addEventListener('scroll', () => {
+    header.classList.toggle('active', windows.scrollY > 0);
+})
